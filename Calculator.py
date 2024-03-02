26d92148efdffddfb3770395a183c6f7fc98a745
Maks <maksbanda@gmail.com>
2024-03-02 16:43:58 +0200

    Add_divide_operations

--- Calculator.py
+++ Calculator.py
@@ -9,6 +9,11 @@ class Calculator:
     def multiply(self, a, b):
         return a * b
     
+    def divide(self, a, b):
+        if b == 0:
+            raise ValueError("Ділення на нуль неможливе")
+        return a / b
+    
 
 class TestCalculator(unittest.TestCase):
     def setUp(self):
@@ -38,6 +43,16 @@ class TestCalculator(unittest.TestCase):
 
         # Assert
         self.assertEqual(result, 15, "Добуток 5 і 3 має бути 15")
+    
+    def test_divide_positive_numbers(self):
+        # Arrange
+        a, b = 5, 3
+
+        # Act
+        result = self.calculator.divide(a, b)
+
+        # Assert
+        self.assertEqual(result, 1.66667, "Результат ділення 5 на 3 має бути 1.66667")
 
 if __name__ == "__main__":
     unittest.main()
